69e9eb981fc0318aabe8e64785c179324120e738
tlkahn <joshgy@gmail.com>
2023-12-15 12:59:03 +0800

    Initial Commit

---
+++ vima/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vima
+//
+//  Created by Josh Kwok on 2023/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vima/vimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vimaApp.swift
+//  vima
+//
+//  Created by Josh Kwok on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct vimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
